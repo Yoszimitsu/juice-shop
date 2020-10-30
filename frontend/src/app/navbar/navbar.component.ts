bf1673e96b242d2d192df63d19753e5a81a60084
yoszimitsu <cotam07@gmail.com>
2020-10-30 14:03:09 +0100

    add sanitizer for track-result and search-navbar

--- frontend/src/app/navbar/navbar.component.ts
+++ frontend/src/app/navbar/navbar.component.ts
@@ -3,19 +3,20 @@
  * SPDX-License-Identifier: MIT
  */
 
-import { environment } from '../../environments/environment'
-import { ChallengeService } from '../Services/challenge.service'
-import { UserService } from '../Services/user.service'
-import { AdministrationService } from '../Services/administration.service'
-import { ConfigurationService } from '../Services/configuration.service'
-import { Component, EventEmitter, NgZone, OnInit, Output } from '@angular/core'
-import { CookieService } from 'ngx-cookie-service'
-import { TranslateService } from '@ngx-translate/core'
-import { Router } from '@angular/router'
-import { SocketIoService } from '../Services/socket-io.service'
-import { LanguagesService } from '../Services/languages.service'
-import { MatSnackBar } from '@angular/material/snack-bar'
-import { BasketService } from '../Services/basket.service'
+import {environment} from '../../environments/environment'
+import {ChallengeService} from '../Services/challenge.service'
+import {UserService} from '../Services/user.service'
+import {AdministrationService} from '../Services/administration.service'
+import {ConfigurationService} from '../Services/configuration.service'
+import {Component, EventEmitter, NgZone, OnInit, Output} from '@angular/core'
+import {CookieService} from 'ngx-cookie-service'
+import {TranslateService, SecurityContext} from '@ngx-translate/core'
+import {Router} from '@angular/router'
+import {SocketIoService} from '../Services/socket-io.service'
+import {LanguagesService} from '../Services/languages.service'
+import {MatSnackBar} from '@angular/material/snack-bar'
+import {BasketService} from '../Services/basket.service'
+import {DomSanitizer} from '@angular/platform-browser'
 
 import {
   faBomb,
@@ -37,11 +38,11 @@ import {
   faUserCircle,
   faUserSecret
 } from '@fortawesome/free-solid-svg-icons'
-import { faComments } from '@fortawesome/free-regular-svg-icons'
-import { faGithub } from '@fortawesome/free-brands-svg-icons'
-import { dom, library } from '@fortawesome/fontawesome-svg-core'
-import { LoginGuard } from '../app.guard'
-import { roles } from '../roles'
+import {faComments} from '@fortawesome/free-regular-svg-icons'
+import {faGithub} from '@fortawesome/free-brands-svg-icons'
+import {dom, library} from '@fortawesome/fontawesome-svg-core'
+import {LoginGuard} from '../app.guard'
+import {roles} from '../roles'
 
 library.add(faLanguage, faSearch, faSignInAlt, faSignOutAlt, faComment, faBomb, faTrophy, faInfoCircle, faShoppingCart, faUserSecret, faRecycle, faMapMarker, faUserCircle, faGithub, faComments, faThermometerEmpty, faThermometerQuarter, faThermometerHalf, faThermometerThreeQuarters, faThermometerFull)
 dom.watch()
@@ -66,13 +67,14 @@ export class NavbarComponent implements OnInit {
 
   @Output() public sidenavToggle = new EventEmitter()
 
-  constructor (private administrationService: AdministrationService, private challengeService: ChallengeService,
-    private configurationService: ConfigurationService, private userService: UserService, private ngZone: NgZone,
-    private cookieService: CookieService, private router: Router, private translate: TranslateService,
-    private io: SocketIoService, private langService: LanguagesService, private loginGuard: LoginGuard,
-    private snackBar: MatSnackBar, private basketService: BasketService) { }
+  constructor(private administrationService: AdministrationService, private challengeService: ChallengeService,
+              private configurationService: ConfigurationService, private userService: UserService, private ngZone: NgZone,
+              private cookieService: CookieService, private router: Router, private translate: TranslateService,
+              private io: SocketIoService, private langService: LanguagesService, private loginGuard: LoginGuard,
+              private snackBar: MatSnackBar, private basketService: BasketService, private sanitizer: DomSanitizer) {
+  }
 
-  ngOnInit () {
+  ngOnInit() {
     this.getLanguages()
     this.basketService.getItemTotal().subscribe(x => this.itemTotal = x)
     this.administrationService.getApplicationVersion().subscribe((version: any) => {
@@ -124,7 +126,7 @@ export class NavbarComponent implements OnInit {
     })
   }
 
-  checkLanguage () {
+  checkLanguage() {
     if (this.cookieService.get('language')) {
       const langKey = this.cookieService.get('language')
       this.translate.use(langKey)
@@ -137,27 +139,30 @@ export class NavbarComponent implements OnInit {
     }
   }
 
-  search (value: string) {
+  search(value: string) {
     if (value) {
-      const queryParams = { queryParams: { q: value } }
+      var sanitizedValue = this.sanitizer.sanitize(SecurityContext.HTML, value);
+      const queryParams = {queryParams: {q: sanitizedValue}}
       this.ngZone.run(() => this.router.navigate(['/search'], queryParams))
     } else {
       this.ngZone.run(() => this.router.navigate(['/search']))
     }
   }
 
-  getUserDetails () {
+  getUserDetails() {
     this.userService.whoAmI().subscribe((user: any) => {
       this.userEmail = user.email
     }, (err) => console.log(err))
   }
 
-  isLoggedIn () {
+  isLoggedIn() {
     return localStorage.getItem('token')
   }
 
-  logout () {
-    this.userService.saveLastLoginIp().subscribe((user: any) => { this.noop() }, (err) => console.log(err))
+  logout() {
+    this.userService.saveLastLoginIp().subscribe((user: any) => {
+      this.noop()
+    }, (err) => console.log(err))
     localStorage.removeItem('token')
     this.cookieService.delete('token', '/')
     sessionStorage.removeItem('bid')
@@ -165,7 +170,7 @@ export class NavbarComponent implements OnInit {
     this.ngZone.run(() => this.router.navigate(['/']))
   }
 
-  changeLanguage (langKey: string) {
+  changeLanguage(langKey: string) {
     this.translate.use(langKey)
     let expires = new Date()
     expires.setFullYear(expires.getFullYear() + 1)
@@ -182,15 +187,15 @@ export class NavbarComponent implements OnInit {
     }
   }
 
-  getScoreBoardStatus () {
-    this.challengeService.find({ name: 'Score Board' }).subscribe((challenges: any) => {
+  getScoreBoardStatus() {
+    this.challengeService.find({name: 'Score Board'}).subscribe((challenges: any) => {
       this.ngZone.run(() => {
         this.scoreBoardVisible = challenges[0].solved
       })
     }, (err) => console.log(err))
   }
 
-  goToProfilePage () {
+  goToProfilePage() {
     window.location.replace(environment.hostServer + '/profile')
   }
 
@@ -199,16 +204,17 @@ export class NavbarComponent implements OnInit {
   }
 
   // tslint:disable-next-line:no-empty
-  noop () { }
+  noop() {
+  }
 
-  getLanguages () {
+  getLanguages() {
     this.langService.getLanguages().subscribe((res) => {
       this.languages = res
       this.checkLanguage()
     })
   }
 
-  isAccounting () {
+  isAccounting() {
     const payload = this.loginGuard.tokenDecode()
     return payload && payload.data && payload.data.role === roles.accounting
   }
